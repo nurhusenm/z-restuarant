ee0dcca7eae626e581fc62f017fd3be5c4c8f9b7
nurhussenm <nuthussenm80@gmail.com>
2025-06-09 18:30:54 +0300

    adding contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className='justify-center align-center'>this is Contact page</h1>
+    </div>
+  );
+};
+
+export default Contact;
+
