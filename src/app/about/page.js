0ac7c2bfbfb74a85606f4c6ecaef6ca9bf5ff34f
nurhussenm <nuthussenm80@gmail.com>
2025-06-09 18:21:09 +0300

    first commit

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='justify-center align-center'>this is About page</h1>
+    </div>
+  );
+};
+
+export default About;
+
---
+++ src/app/menu/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <div>
+      <h1 className='justify-center align-center'>this is Menu page</h1>
+    </div>
+  );
+};
+
+export default Menu;
+
